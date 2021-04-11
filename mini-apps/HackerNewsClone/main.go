c11869c6da967d904443bcf2d3978f3ed354e277
Nipun Chawla <nipun.chawla@san-00614.lan>
2021-04-11 16:05:06 -0700

    update cache loading faster; use reciever

--- mini-apps/HackerNewsClone/main.go
+++ mini-apps/HackerNewsClone/main.go
@@ -33,19 +33,52 @@ func main() {
 
 }
 
+type storyCache struct {
+	numOfStories int
+	cache        []item
+	expiration   time.Time
+	duration     time.Duration
+	mutex        sync.Mutex
+}
+
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+
+	sc := storyCache{
+		numOfStories: numStories,
+		duration:     6 * time.Second,
+	}
+	// ticker to update the stories after every 3 seconds
+	// reciever updates the struct
+	go func() {
+		ticker := time.NewTicker(3 * time.Second)
+		for {
+			//<-ticker.C moved this down
+			temp := storyCache{
+				numOfStories: numStories,
+				duration:     6 * time.Second,
+			}
+			temp.getCachedStoriesFaster()
+			sc.mutex.Lock()
+			sc.cache = temp.cache
+			//fmt.Println(temp.cache)
+			sc.expiration = temp.expiration
+			sc.mutex.Unlock()
+			<-ticker.C
+		}
+
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		var stories []item
-		stories, err := getCachedStories(numStories)
-
+		stories, err := sc.getCachedStoriesFaster()
+		//log.Println(stories)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		data := templateData{
-			Stories: stories,
-			Time:    time.Now().Sub(start),
+			Stories:         stories,
+			Time:            time.Now().Sub(start),
 			NumberOfStories: numStories,
 		}
 		err = tpl.Execute(w, data)
@@ -56,8 +89,7 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	})
 }
 
-
-func getTopStories(numStories int) ([] item,error){
+func getTopStories(numStories int) ([]item, error) {
 	var client hn.Client
 	ids, err := client.TopItems()
 	if err != nil {
@@ -68,25 +100,44 @@ func getTopStories(numStories int) ([] item,error){
 	var stories []item
 
 	at := 0
-	for len(stories) < numStories{
+	for len(stories) < numStories {
 		//fmt.Println("loop")
-		need := (numStories - len(stories)) * 5/4
+		need := (numStories - len(stories)) * 5 / 4
 		stories = append(stories, getStories(ids[at:at+need])...)
-		at+=need
+		at += need
 	}
 
 	return stories[:numStories], nil
 }
+
 var (
-	cache []item
+	cache           []item
 	cacheExpiration time.Time
-	cacheMutex sync.Mutex
+	cacheMutex      sync.Mutex
 )
 
-func getCachedStories(numStories int) ([]item, error)  {
+// new version reciever on type storyCache so it can be called on that type
+func (sc *storyCache) getCachedStoriesFaster() ([]item, error) {
+	sc.mutex.Lock()
+	defer sc.mutex.Unlock()
+	if time.Now().Sub(sc.expiration) < 0 {
+		return sc.cache, nil
+	}
+	stories, err := getTopStories(sc.numOfStories)
+	if err != nil {
+		return nil, err
+	}
+	sc.cache = stories
+	sc.expiration = time.Now().Add(sc.duration) // set cache timing
+	return sc.cache, err
+}
+
+// old version -- simple
+// keeping for reference
+func getCachedStories(numStories int) ([]item, error) {
 	cacheMutex.Lock()
 	defer cacheMutex.Unlock()
-	if time.Now().Sub(cacheExpiration) < 0  {
+	if time.Now().Sub(cacheExpiration) < 0 {
 		return cache, nil
 	}
 	stories, err := getTopStories(numStories)
@@ -99,38 +150,38 @@ func getCachedStories(numStories int) ([]item, error)  {
 }
 
 // adding concurrency
-func getStories(ids []int) [] item {
+func getStories(ids []int) []item {
 
 	type result struct {
-		idx int
+		idx  int
 		item item
-		err error
+		err  error
 	}
 	resultCh := make(chan result)
 	var stories []item
 	for idx, id := range ids {
-		go func(idx,id int) {
+		go func(idx, id int) {
 			var client hn.Client // avoids race conditions for multiple go routines
 			hnItem, err := client.GetItem(id)
-			if  err != nil {
-				resultCh <- result{idx : idx, err: err}
+			if err != nil {
+				resultCh <- result{idx: idx, err: err}
 			}
 			resultCh <- result{idx: idx, item: parseHNItem(hnItem)}
 		}(idx, id)
 	}
 
 	var results []result
-	for i:=0; i < len(ids); i++ {
+	for i := 0; i < len(ids); i++ {
 		results = append(results, <-resultCh)
 	}
 	sort.Slice(results, func(i, j int) bool {
 		return results[i].idx < results[j].idx
 	})
-	for _,res :=range results{
-		if res.err != nil{
+	for _, res := range results {
+		if res.err != nil {
 			continue
 		}
-		if isStoryLink(res.item){
+		if isStoryLink(res.item) {
 			stories = append(stories, res.item)
 		}
 	}
@@ -158,7 +209,7 @@ type item struct {
 }
 
 type templateData struct {
-	Stories []item
-	Time    time.Duration
+	Stories         []item
+	Time            time.Duration
 	NumberOfStories int
 }

f018cf980689dae1799ccf35c40c82bcb57a343a
Nipun Chawla <nipun.chawla@san-00614.lan>
2021-04-11 15:10:29 -0700

    add hacker news server

---
+++ mini-apps/HackerNewsClone/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+
+	"HackerNewsClone/hn"
+)
+
+func main() {
+	// parse flags
+	var port, numStories int
+	flag.IntVar(&port, "port", 4000, "the port to start the web server on")
+	flag.IntVar(&numStories, "num_stories", 30, "the number of top stories to display")
+	flag.Parse()
+
+	tpl := template.Must(template.ParseFiles("./index.gohtml"))
+
+	http.HandleFunc("/", handler(numStories, tpl))
+	log.Println("serving / handler")
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+
+}
+
+func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		var stories []item
+		stories, err := getCachedStories(numStories)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data := templateData{
+			Stories: stories,
+			Time:    time.Now().Sub(start),
+			NumberOfStories: numStories,
+		}
+		err = tpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Failed to process the template", http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+
+func getTopStories(numStories int) ([] item,error){
+	var client hn.Client
+	ids, err := client.TopItems()
+	if err != nil {
+		//http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+		return nil, errors.New("failed to get top stories")
+	}
+
+	var stories []item
+
+	at := 0
+	for len(stories) < numStories{
+		//fmt.Println("loop")
+		need := (numStories - len(stories)) * 5/4
+		stories = append(stories, getStories(ids[at:at+need])...)
+		at+=need
+	}
+
+	return stories[:numStories], nil
+}
+var (
+	cache []item
+	cacheExpiration time.Time
+	cacheMutex sync.Mutex
+)
+
+func getCachedStories(numStories int) ([]item, error)  {
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+	if time.Now().Sub(cacheExpiration) < 0  {
+		return cache, nil
+	}
+	stories, err := getTopStories(numStories)
+	if err != nil {
+		return nil, err
+	}
+	cache = stories
+	cacheExpiration = time.Now().Add(1 * time.Second) // set cache timing
+	return stories, err
+}
+
+// adding concurrency
+func getStories(ids []int) [] item {
+
+	type result struct {
+		idx int
+		item item
+		err error
+	}
+	resultCh := make(chan result)
+	var stories []item
+	for idx, id := range ids {
+		go func(idx,id int) {
+			var client hn.Client // avoids race conditions for multiple go routines
+			hnItem, err := client.GetItem(id)
+			if  err != nil {
+				resultCh <- result{idx : idx, err: err}
+			}
+			resultCh <- result{idx: idx, item: parseHNItem(hnItem)}
+		}(idx, id)
+	}
+
+	var results []result
+	for i:=0; i < len(ids); i++ {
+		results = append(results, <-resultCh)
+	}
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].idx < results[j].idx
+	})
+	for _,res :=range results{
+		if res.err != nil{
+			continue
+		}
+		if isStoryLink(res.item){
+			stories = append(stories, res.item)
+		}
+	}
+	return stories
+
+}
+
+func isStoryLink(item item) bool {
+	return item.Type == "story" && item.URL != ""
+}
+
+func parseHNItem(hnItem hn.Item) item {
+	ret := item{Item: hnItem}
+	url, err := url.Parse(ret.URL)
+	if err == nil {
+		ret.Host = strings.TrimPrefix(url.Hostname(), "www.")
+	}
+	return ret
+}
+
+// item is the same as the hn.Item, but adds the Host field
+type item struct {
+	hn.Item
+	Host string
+}
+
+type templateData struct {
+	Stories []item
+	Time    time.Duration
+	NumberOfStories int
+}
